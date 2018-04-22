134e9edbd91fcc2aeaf7adac0ef11110df4ae0ba
KronusTaur <kablokov@gmail.com>
2018-04-22 21:27:17 +0300

    %файлы расчёта%

---
+++ RasCod.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 16 11:53:29 2018
+
+@author: Администратор
+"""
+
+import numpy as nm
+import matplotlib as mp
+import matplotlib.pyplot as plt
+from math import exp
+from math import *
+from math import sqrt
+from numpy import linspace
---
+++ RasShif.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 16 11:53:40 2018
+
+@author: Администратор
+"""
+
